6d79cf446e5e384dc3ca57f8119111dd9dccd0fd
Joshua Graham <joshua.graham@jpginc.com.au>
2014-07-10 15:46:58 +1000

    dev update

--- www/js/main.js
+++ www/js/main.js
@@ -1,13 +1,51 @@
 var controller = (function() {
     var errorReportingLevel = 0;
     var isInitialized = false;
+    var view = new View();
+    var loading = false;
 
-    function errorLogging(toLog, priority) {
+    function loadPage(event, data) {
+        var page;
+        var hash;
+
+        if(loading) {
+            view.abort();
+        }
+
+        if(typeof data.toPage === "string") { 
+            log("to page: "  + data.toPage, 1);
+            hash = $.mobile.path.parseUrl(data.toPage).hash.substring(1);
+
+            if((page = document.getElementById(hash)) === null) {
+                $.mobile.loading("show");
+                event.preventDefault();
+                view.getPage(hash, data, dataReady, loadingFailed);
+            }
+        }
+        return;
+    }
+
+    function dataReady(page) {
+        $.mobile.loading("hide");
+        loading = false;
+        log(page.html());
+        $.mobile.pageContainer.pagecontainer("change", page);
+        return;
+    }
+
+    function loadingFailed(data, errorMessage) {
+        $.mobile.loading("hide");
+        alert(errorMessage);
+        loading = false;
+        return;
+    }
+
+    function log(toLog, priority) {
         if(priority === undefined || errorReportingLevel < priority) {
             console.log(toLog);
-            return true
+            return true;
         }
-        return false
+        return false;
     }
 
     function initialize() {
@@ -15,20 +53,29 @@ var controller = (function() {
             return;
         }
         isInitialized = true;
+        $( document ).on( "pagecontainerbeforechange", loadPage);
+        $.mobile.linkBindingEnabled = true;
+        $.mobile.ajaxEnabled = true;
+
         var test1 = new Date();
         var test2 = new Date();
-        alert(niceString(dayHourMinSec(test1.getTime(), test2.getTime() + 1234211)));
+        //alert(niceString(dayHourMinSec(test1.getTime(), test2.getTime() + 1234211)));
         return;
     }
     
     return {
-        log: errorLogging,
+        log: log,
         initialize: initialize
     };
 })();
 
-// will only work on mobile devices
-document.addEventListener("deviceready", controller.initialize, false);
+if (navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry)/)) {
+    // will only work on mobile devices
+    document.addEventListener("deviceready", controller.initialize, false);
+} else {
+    //for desktop
+    $(document).ready(controller.initialize);
+}
 var youLoseUser = (function() {
     var data;
     var controller;
@@ -100,4 +147,46 @@ function niceString(dhmsArray) {
         }
     }
     return dhmsArray[0] + "D : " + dhmsArray[1] + "H : " + dhmsArray[2] + "m : " + dhmsArray[3] + "s";
+}
+function View() {
+    var menu;
+    var menuItems = ["Me","Stats","Broadcast","Friends","More"];
+    var menuIcons = ["user","cloud","audio","heart","plus"];
+    
+    function getPage(requestedPage, dataObject, successCallback, failCallback) {
+        var newPage = getDiv(requestedPage, "page").append(
+                getDiv(requestedPage, "content").append(getMenu()));
+        $('body').append(newPage);
+        successCallback(newPage);
+        return newPage;
+    }
+
+    function getMenu() {
+        var navbar;
+        if(!menu) {
+            menu = getDiv("menu", "footer").attr("data-position","fixed").append(
+                    getDiv("navbar", "navbar").append(getUL));
+        }
+        return menu.clone();
+    }
+
+    function getUL() {
+        var ul = $("<ul>");
+        var li;
+        for(var i = 0; i < menuItems.length; i++) {
+            li = $("<li>");
+            li.append($("<a>", {href: menuItems[i] + ".html",
+                "data-icon": menuIcons[i], "data-role":"button"}));
+            ul.append(li);
+        }
+        return ul;
+    }
+
+    function getDiv(id, dataRole) {
+        return $("<div>", {id: id, "data-role": dataRole});
+    }
+    return {
+        getPage: getPage,
+        abort: true
+    };
 }
