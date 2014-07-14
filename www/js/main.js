c0a8ca11615254abed0b6bd198cd323ecc4c51bc
Joshua Graham <joshua.graham@jpginc.com.au>
2014-07-14 21:47:24 +1000

    dev update

--- www/js/main.js
+++ www/js/main.js
@@ -462,6 +462,41 @@ function getDiv(id, dataRole) {
 function getElement(type, attrs) {
     return $(document.createElement(type)).attr(attrs);
 }
-localData = (function() {
+function LocalData() {
+    var loadAtStartup = ["user"]; 
+    var allEntries = $.merge([], loadAtStartup);
+    var savedData;
 
-})();
+    this.initialize = function() {
+        savedData = {};
+        for(var i = 0; i < loadAtStartup.length; i++) {
+            try {
+                savedData[loadAtStartup[i]] =
+                    $.parseJSON(window.localStorage.getItem(loadAtStartup[i]));
+            } catch(err) {
+                controller.log("error parsing json!", 8);
+            }
+        }
+        return this;
+    };
+
+    this.get = function(key) {
+        if(!savedData) {
+                this.initialize();
+            }
+        controller.log("getting " +key, 1);
+        controller.log("has " + savedData[key], 1);
+        return savedData[key];
+    };
+
+    this.save = function(key, value) {
+        if(!savedData) {
+            this.initialize();
+        }
+        savedData[key] = value;
+        window.localStorage.setItem(key, JSON.stringify(value));
+        return this;
+    };
+
+    return this;
+}
